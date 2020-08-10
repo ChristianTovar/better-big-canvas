df935587a6ce9a8cff877a9f65c30a39b3b823af
ChristianTovar <tovarchristian21@gmail.com>
2020-08-09 22:43:20 -0500

    Add basic canvas components grid

---
+++ lib/better_big_canvas_web/live/canvas_component.ex
@@ -0,0 +1,9 @@
+defmodule BetterBigCanvasWeb.CanvasComponent do
+  use Phoenix.LiveComponent
+
+  def render(assigns) do
+    ~L"""
+    <div> [ ] </div>
+    """
+  end
+end
--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule BetterBigCanvas.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:elixir_uuid, "~> 1.2"}
     ]
   end
 
