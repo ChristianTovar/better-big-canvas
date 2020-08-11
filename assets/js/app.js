b2d92a388ef181668a5d36459ffcc3545cd889d5
ChristianTovar <tovarchristian21@gmail.com>
2020-08-10 22:19:50 -0500

    Add click coordinates metadata

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,17 @@ import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken},
+  metadata: {
+    click: (e, el) => {
+      return {
+        offsetX: e.offsetX,
+        offsetY: e.offsetY
+      }
+    }
+  }
+})
 
 // Show progress bar on live navigation and form submits
 window.addEventListener("phx:page-loading-start", info => NProgress.start())
