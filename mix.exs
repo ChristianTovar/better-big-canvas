a8495f3dbfcb344b61addfd8c3635d04426239d4
ChristianTovar <tovarchristian21@gmail.com>
2021-11-05 12:51:41 -0400

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -33,14 +33,14 @@ defmodule BetterBigCanvas.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.4"},
-      {:phoenix_live_view, "~> 0.14.2"},
+      {:phoenix, "~> 1.6"},
+      {:phoenix_live_view, "~> 0.17.5"},
       {:floki, ">= 0.0.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 3.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.2"},
       {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
+      {:telemetry_poller, "~> 0.5.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
